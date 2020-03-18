b52df816a9b58a4104dca9240ab75297ab0a888b
akkuratnov98 <akkuratnov.98@yandex.ru>
2020-03-18 18:44:31 +0300

    Fixed script, path.

--- LAB3/server1.1.js
+++ LAB3/server1.1.js
@@ -3,24 +3,23 @@ const fs = require("fs");
 const http = require("http");
 const querystring = require('querystring');
 const URL = require('url');
+const path = require('path');
 
 function start() {
     function onRequest(request, response) {
         console.log("Request received");
-        const page = fs.readFileSync('lab2.html');
+        const page = fs.readFileSync(path.resolve(__dirname, 'lab2.html'));
 
         if (URL.parse(request.url).search != null) {
             var queryVal = querystring.parse(URL.parse(request.url).query);                     
-            request.on('data', function (data) {
-                    
-            });
+            request.on('data', () => { });
 
-            request.on('end',function() {
+            request.on('end', () => {
                 response.writeHead(200);                
                 response.write(
                     JSON.stringify({ 
                        amount : isNaN(filterInt(queryVal.param1) + filterInt(queryVal.param2)) ? "Error!" : 
-                       ' Сумма = ' + (Number.parseInt(queryVal.param1) + Number.parseInt(queryVal.param2))
+                       `Сумма = ${Number.parseInt(queryVal.param1) + Number.parseInt(queryVal.param2)}`
                     })
                 );
                 response.end();
